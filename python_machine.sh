badd101c2a6a3577837efe5e145d568e4980f21d
andr435 <andr435@gmail.com>
2025-03-29 03:44:48 +0300

    use_2to3 bug fix

--- python_machine.sh
+++ python_machine.sh
@@ -5,19 +5,18 @@
 # Purpose: Install python 3 packages, virtual enviroment manager.
 # Create and activate virtual enviroment in given directory or in current directory if nothing given.
 # Date: 21-03-2025
-# Version: 1.0.0
+# Version: 1.1.0
 ################################################
 set -o errexit
 set -o pipefail
 set -o nounset
 ################################################
 
-script_version=1.0.0
+script_version=1.1.0
 script_name=python_machine.sh
 work_directory="."
 default_venv_directory=".venv"
 real_user=${SUDO_USER:-$(whoami)}
-run_user=$(whoami)
 
 . /etc/os-release
 
@@ -27,7 +26,6 @@ Destruct(){
     unset work_directory
     unset default_venv_directory
     unset real_user
-    unset run_user
 
     return 0    
 }
@@ -52,11 +50,13 @@ Install_packages(){
         Install_package "$item"
     done
 
-    /usr/bin/crb
+    /usr/bin/crb enable
     
     for item in "${p_modules[@]}"; do
         Install_package "$item"
     done
+	
+    python3 -m pip install --upgrade pip setuptools wheel
 
     local loop_again="true"
 
@@ -72,14 +72,14 @@ Choose virtual enviroment maneger for project: " venv_manager
             Create_venv_venv
         elif [[ $venv_manager == "2" ]]; then
             loop_again="false" 
-            python3 -m pip install --upgrade pip
             sudo -u "$real_user" python3.9 -m pip install -U pipenv
-	    export PATH=$PATH:home/${real_user}/.local/bin
+	        export PATH=$PATH:home/${real_user}/.local/bin
             Create_venv_pipenv
         elif [[ $venv_manager == "3" ]]; then
             loop_again="false"
             sudo -u "$real_user" pipx install poetry
 	        sudo -u "$real_user" pipx ensurepath
+		    export PATH
             Create_venv_poetry
         else 
             echo "incorect choise"
@@ -111,12 +111,14 @@ Create_work_directory(){
     fi
 
     sudo -u "$real_user" mkdir -p ${work_directory} 
+
+    return 0
 }
 
 Create_venv_venv(){
     local venv_path=${work_directory}/"$default_venv_directory"
     Create_work_directory
-    sudo -u "$real_user" python -m venv {$venv_path}
+    sudo -u "$real_user" python -m venv ${venv_path}
 
     return 0
 }
@@ -136,23 +138,29 @@ Create_venv_poetry(){
     local project_name="demo"
     read -p "Project name? " project_name
     cd "$work_directory"
-    sudo -u "$real_user" poetry new "$project_name"
-    sudo -u "$real_user" eval $(poetry env activate)
-
+    sudo -u "$real_user" /home/${real_user}/.local/bin/poetry new "$project_name"
+    su -c "eval $(/home/${real_user}/.local/bin/poetry env activate)" "$real_user"
+    cd "$project_name"
     return 0
 }
 
 Install_project_packages(){
+    # depricated with 'use_2to3' dependecy, install first
+    local packages_depricated="dictalchemy Flask-AlchemyView"
     local project_pack="flask flask-sqlalchemy flask-alchemyview bootstrap-flask quart db-sqlite3"
     local venv_path=${work_directory}/"$default_venv_directory"
 
     if [[ $venv_manager == "1" ]]; then
+	    sudo -u "$real_user" "$venv_path"/bin/python -m pip install "setuptools<58.0.0"
+	    sudo -u "$real_user" "$venv_path"/bin/python -m pip install ${packages_depricated}
         sudo -u "$real_user" "$venv_path"/bin/python -m pip install ${project_pack}
-	echo "To activate virtual enviroment run command: ${venv_path}/bin/activate"
+	    echo "To activate virtual enviroment run command: ${venv_path}/bin/activate"
     elif [[ $venv_manager == "2" ]]; then
         su -c "/home/${real_user}/.local/bin/pipenv install ${project_pack}" "$real_user"
     elif [[ $venv_manager == "3" ]]; then
-        sudo -u "$real_user" poetry add ${project_pack[@]}
+	    sudo -u "$real_user" /home/${real_user}/.local/bin/poetry run pip install "setuptools<58.0.0"
+	    sudo -u "$real_user" /home/${real_user}/.local/bin/poetry run pip install "${packages_depricated}"
+        sudo -u "$real_user" /home/${real_user}/.local/bin/poetry add ${project_pack}
     fi
 
     return 0
@@ -173,7 +181,7 @@ DESCRIPTION
  OPTIONS
 	-h|--help          Print this help
 	-v|--version       Show version
-        -d|--directory     Work directory. Create vitrual enviroment in this directory
+    -d|--directory     Work directory. Create vitrual enviroment in this directory
 
  	"
 
